ae061dc7f33779bc0574c9336fac3cfcfcdfdbb5
Matias Monegal <matias.monegal@hotmail.com>
2018-07-04 11:58:22 -0300

    Update tablero de juegos
    +Añadido tablero de juego
    +Añadido fichas de juego
    +Añadido botón para refrescar tabla

--- src/js/gameManager.js
+++ src/js/gameManager.js
@@ -5,6 +5,7 @@ var tablero = [
     [],
     [],
     [],
+    []
 ];
 var player1Name, player2Name, currentGameID, currentLocalStorageContent;
 var numeroFila, numeroColumna;
@@ -14,30 +15,38 @@ var timer = new Timer();
 timer.start();
 
 $(document).ready(function() {
-    //Constructor del tablero
-    for (var i = 0; i < 6; i++) {
+    //Constructor array tablero
+    for (var i = 0; i < 7; i++) {
         for (var j = 0; j < 6; j++) {
             numeroPos = numeroPos + 1
             tablero[i][j] = numeroPos;
         }
     }
-    //Fin constructor de tablero
+    //Fin constructor array tablero
+
     //Get del contenido del localStorage
     currentGameID = sessionStorage["4line_current_session_ID"];
     currentLocalStorageContent = JSON.parse(localStorage.getItem("4line_games_sessions_ID:" + currentGameID));
     player1Name = currentLocalStorageContent[0].currentGameSession.setupData.player1Name;
     player2Name = currentLocalStorageContent[0].currentGameSession.setupData.player2Name;
+
     //Set del contenido del localStorage
     $('#currentSessionID').text(currentGameID);
     $('#jugador1').text(player1Name);
     $('#jugador2').text(player2Name);
+
+    $('#jugador1').css('color', 'lightgreen');
+    $('#jugador2').css('color', 'red');
+
 });
 
+//Inicializador Timer
 timer.addEventListener('secondsUpdated', function(e) {
     $('#currentTime').html(timer.getTimeValues().toString());
 });
 
 $(document).ready(function() {
+    //Creador tabla de juego
     var boardBody = document.getElementById("boardBody");
     var contador = 0;
     for (var i = 0; i < tablero.length; i++) {
@@ -50,4 +59,5 @@ $(document).ready(function() {
             contador++;
         }
     }
+    //Fin creador tabla de juego
 });

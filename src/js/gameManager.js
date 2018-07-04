905e55e696878300249556ca758e3469381f5994
Matias Monegal <matias.monegal@hotmail.com>
2018-07-03 22:48:55 -0300

    Update mostrar tabla
    +Crea y muestra la tabla de 6x7 en pantalla

--- src/js/gameManager.js
+++ src/js/gameManager.js
@@ -5,21 +5,23 @@ var tablero = [
     [],
     [],
     [],
-    []
 ];
 var player1Name, player2Name, currentGameID, currentLocalStorageContent;
-var numeroPos, numeroFila, numeroColumna;
+var numeroFila, numeroColumna;
+var numeroPos = 0;
 
 var timer = new Timer();
 timer.start();
 
 $(document).ready(function() {
     //Constructor del tablero
-    for (var i = 0; i < 7; i++) {
-        for (var j = 0; j < 7; j++) {
-            tablero[i][j] = 0;
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 6; j++) {
+            numeroPos = numeroPos + 1
+            tablero[i][j] = numeroPos;
         }
-    } //Fin constructor de tablero
+    }
+    //Fin constructor de tablero
     //Get del contenido del localStorage
     currentGameID = sessionStorage["4line_current_session_ID"];
     currentLocalStorageContent = JSON.parse(localStorage.getItem("4line_games_sessions_ID:" + currentGameID));
@@ -37,26 +39,15 @@ timer.addEventListener('secondsUpdated', function(e) {
 
 $(document).ready(function() {
     var boardBody = document.getElementById("boardBody");
-    var i = 0;
-    while (i < tablero.length) {
-        for (var j = 0; j < tablero.length; j++) {
+    var contador = 0;
+    for (var i = 0; i < tablero.length; i++) {
+        contador = 0;
+        var fila = boardBody.insertRow(cont);
+        for (var j = 0; j < 6; j++) {
             var cont = boardBody.rows.length;
-            var fila = boardBody.insertRow(cont);
-            var celda1 = fila.insertCell(0);
-            celda1.innerText = "0";
-            var celda2 = fila.insertCell(1);
-            celda2.innerText = "0";
-            var celda3 = fila.insertCell(2);
-            celda3.innerText = "0";
-            var celda4 = fila.insertCell(3);
-            celda4.innerText = "0";
-            var celda5 = fila.insertCell(4);
-            celda5.innerText = "0";
-            var celda6 = fila.insertCell(5);
-            celda6.innerText = "0";
-            var celda7 = fila.insertCell(6);
-            celda7.innerText = "0";
-            i++;
+            var celda = fila.insertCell(contador);
+            celda.innerText = tablero[i][j];
+            contador++;
         }
     }
 });

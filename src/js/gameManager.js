78f50b35af280d23355eed8090e77060654ca47b
Matias Monegal <matias.monegal@hotmail.com>
2018-07-08 21:34:27 -0300

    Update - Corrección tabla de juego
    +Agregado botones para colocar fichas de juegos
    +Corregida la tabla de juego, ahora es de 6x7

--- src/js/gameManager.js
+++ src/js/gameManager.js
@@ -7,6 +7,13 @@ var tablero = [
     [],
     []
 ];
+
+var fichas = [{
+    player1: 'assets/Ficha1.png'
+}, {
+    player2: 'assets/Ficha2.png'
+}];
+
 var player1Name, player2Name, currentGameID, currentLocalStorageContent;
 var numeroFila, numeroColumna;
 var numeroPos = 0;
@@ -18,10 +25,13 @@ $(document).ready(function() {
     //Constructor array tablero
     for (var i = 0; i < 7; i++) {
         for (var j = 0; j < 6; j++) {
-            numeroPos = numeroPos + 1
-            tablero[i][j] = numeroPos;
+           // numeroPos = numeroPos + 1
+           // tablero[i][j] = numeroPos;
+           tablero[i][j] = 0;
         }
     }
+
+
     //Fin constructor array tablero
 
     //Get del contenido del localStorage
@@ -35,29 +45,13 @@ $(document).ready(function() {
     $('#jugador1').text(player1Name);
     $('#jugador2').text(player2Name);
 
+    //Set de colores de los nombres de los jugadores
     $('#jugador1').css('color', 'lightgreen');
     $('#jugador2').css('color', 'red');
-
 });
+
 
 //Inicializador Timer
 timer.addEventListener('secondsUpdated', function(e) {
     $('#currentTime').html(timer.getTimeValues().toString());
-});
-
-$(document).ready(function() {
-    //Creador tabla de juego
-    var boardBody = document.getElementById("boardBody");
-    var contador = 0;
-    for (var i = 0; i < tablero.length; i++) {
-        contador = 0;
-        var fila = boardBody.insertRow(cont);
-        for (var j = 0; j < 6; j++) {
-            var cont = boardBody.rows.length;
-            var celda = fila.insertCell(contador);
-            celda.innerText = tablero[i][j];
-            contador++;
-        }
-    }
-    //Fin creador tabla de juego
 });
